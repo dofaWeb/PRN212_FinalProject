cdee4c9c91dc5ee10b1b3dd9b35a981aa100cd25
Vaiken <phucnvce170116@fpt.edu.vn>
2024-10-23 15:39:49 +0700

    Add Supplier

--- PRN212_FinalProject/ViewModel/SupllierViewModel.cs
+++ PRN212_FinalProject/ViewModel/SupllierViewModel.cs
@@ -1,6 +1,7 @@
 ﻿
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using PRN212_FinalProject.Entities;
+using PRN212_FinalProject.Helper;
 using PRN212_FinalProject.ViewModel;
 using System;
 using System.Collections.Generic;
@@ -9,15 +10,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace PRN212_FinalProject.ViewModel
 {
     internal class SupplierViewModel : BaseViewModel
     {
         public ObservableCollection<Supplier> Suppliers { get; set; }
+        public ICommand AddButton {  get; set; }
+        public void AddSupplier(Object Parameter)
+        {
+            DBContext dBContext = new DBContext();
+            Supplier Addsupplier;
+            Addsupplier = new Supplier();
+            Addsupplier.Id = _supplierId;
+            Addsupplier.Name = _supplierName;
+            Addsupplier.ContactInfo = _supplierContactInfo;
+            Addsupplier.Address = _supplierAddress;
+            dBContext.Suppliers.Add(Addsupplier);
+            dBContext.SaveChanges();
+            LoadData();
+        }
         public SupplierViewModel()
+
         {
             LoadData();
+            AddButton = new RelayCommand(AddSupplier);
         }
         void LoadData()
         {
@@ -25,6 +43,56 @@ namespace PRN212_FinalProject.ViewModel
             Suppliers = new ObservableCollection<Supplier>(dBContext.Suppliers.ToList());
         }
 
+        private string _supplierId;
+        private string _supplierName;
+        private string _supplierContactInfo;
+        private string _supplierAddress;
 
+        public string SupplierId
+        {
+            get { return _supplierId; }
+            set
+            {
+                _supplierId = value;
+                OnPropertyChanged(SupplierId);
+            }
+        }
+        public string SupplierName
+        {
+            get { return _supplierName; }
+            set
+            {
+                _supplierName = value;
+                OnPropertyChanged(SupplierName);
+            }
+        }
+        public string SupplierContactInfo
+        {
+            get { return _supplierContactInfo; }
+            set
+            {
+                _supplierContactInfo = value; OnPropertyChanged(SupplierContactInfo);
+            }
+        }
+        public string SupplierAddress
+        {
+            get { return _supplierAddress; }
+            set { _supplierAddress = value; 
+                OnPropertyChanged(SupplierAddress); }
+        }
+
+        private Supplier _showItem;
+        public Supplier ShowItem
+        {
+            get { return _showItem; }
+            set
+            {
+
+            }
+
+
+
+
+        }   
     }
 }
