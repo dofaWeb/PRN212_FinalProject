f73d33c9b71689d69bfa98f1569621c8effe4c8d
dofaWeb <gioakim0432004@gmail.com>
2024-10-25 15:14:24 +0700

    Merge

--- PRN212_FinalProject/obj/Debug/net8.0-windows/Login.g.i.cs
+++ PRN212_FinalProject/obj/Debug/net8.0-windows/Login.g.i.cs
@@ -54,7 +54,7 @@ namespace PRN212_FinalProject {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PRN212_FinalProject;component/login.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PRN212_FinalProject;V1.0.0.0;component/login.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Login.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
