1c70c05620f8aa502b61a6362e9b456e979eef19
dofaWeb <gioakim0432004@gmail.com>
2024-10-30 08:50:48 +0700

    CRUD for Category

--- PRN212_FinalProject/Entities/Category.cs
+++ PRN212_FinalProject/Entities/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PRN212_FinalProject.Entities;
 
@@ -10,4 +11,7 @@ public partial class Category
     public string Name { get; set; } = null!;
 
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+
+    [NotMapped]
+    public string? CategoryType { get; set; }
 }
