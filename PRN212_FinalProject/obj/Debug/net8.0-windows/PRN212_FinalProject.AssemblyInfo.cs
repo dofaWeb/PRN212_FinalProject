25e45207790927345dbfe32c70ea9ad6d4444c90
dofaWeb <gioakim0432004@gmail.com>
2024-11-04 09:15:36 +0700

    Database diagram

--- PRN212_FinalProject/obj/Debug/net8.0-windows/PRN212_FinalProject.AssemblyInfo.cs
+++ PRN212_FinalProject/obj/Debug/net8.0-windows/PRN212_FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRN212_FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66ab0b84a441c4937b275e95db63474296800bf6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+326c5a622e787ba5fec40a3e08096733127e5a80")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRN212_FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRN212_FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
